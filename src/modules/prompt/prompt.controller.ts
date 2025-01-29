4c6918b5b071487c4697751c06c8748915ba94c4
Haruki <aratashiharuki@gmail.com>
2025-01-29 21:55:20 +0300

    divide non streamable from streamable

--- src/modules/prompt/prompt.controller.ts
+++ src/modules/prompt/prompt.controller.ts
@@ -1,15 +1,16 @@
 import {
-  BadGatewayException,
   Body,
   Controller,
-  InternalServerErrorException,
+  HttpException,
+  HttpStatus,
   Post,
-  Sse,
+  Res,
 } from '@nestjs/common';
 
 import { BalancerService } from '../balancer/balancer.service';
 import { QwenService } from '../qwen/qwen.service';
 import { finalize, from, map } from 'rxjs';
+import { Response } from 'express';
 import { QwenCompletionDto } from '../qwen/dto/completion.dto';
 
 @Controller('prompt')
@@ -19,31 +20,60 @@ export class PromptController {
     private readonly qwenService: QwenService,
   ) {}
 
-  @Post('/completions')
-  @Sse()
-  async completions(@Body() dto: QwenCompletionDto) {
+  @Post('completions')
+  async completions(@Body() dto: QwenCompletionDto, @Res() res: Response) {
     const credentials = await this.balancerService.findAndClaim();
-
     if (!credentials) {
-      throw new BadGatewayException('No available credentials');
+      throw new HttpException(
+        'No available credentials',
+        HttpStatus.BAD_GATEWAY,
+      );
     }
 
     const stream = this.qwenService.getCompletion(
       credentials.token as string,
       dto,
     );
-
     if (!stream) {
-      throw new InternalServerErrorException('Error during completion');
+      throw new HttpException(
+        'Error during completion',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
 
-    return from(stream).pipe(
-      map((chunk) => {
-        return { data: chunk };
-      }),
-      finalize(() => {
-        this.balancerService.release(credentials.login);
-      }),
-    );
+    if (dto.stream) {
+      res.setHeader('Content-Type', 'text/event-stream');
+      res.setHeader('Cache-Control', 'no-cache');
+      res.setHeader('Connection', 'keep-alive');
+      res.setHeader('Transfer-Encoding', 'chunked');
+
+      from(stream)
+        .pipe(
+          map((chunk) => {
+            console.log(JSON.stringify(chunk));
+            return JSON.stringify(chunk);
+          }),
+          finalize(() => {
+            this.balancerService.release(credentials.login);
+          }),
+        )
+        .subscribe({
+          next: (chunk) => {
+            res.write(`data: ${chunk}\n\n`);
+          },
+          error: (err) => {
+            console.error('Stream error:', err);
+            res.end();
+          },
+          complete: () => {
+            res.end();
+          },
+        });
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+
+      res.write(JSON.stringify((await stream.next()).value));
+      res.end();
+    }
   }
 }
